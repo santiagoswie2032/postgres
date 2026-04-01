689531764c1da2b88c486d5ef384396fecec725a
santiago <vikalpbordekar@gmail.com>
2026-04-01 18:30:57 +0000

    fixing the tables

--- src/db.js
+++ src/db.js
@@ -10,7 +10,6 @@ db.exec(`
     CREATE TABLE users ( 
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE,
-
     password TEXT
     
 )
@@ -25,7 +24,6 @@ db.exec(`
         CREATE TABLE todos ( 
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER,
-        username TEXT, 
         task TEXT,
         completed BOOLEAN DEFAULT 0,
         FOREIGN KEY(user_id) REFERENCES users (id)
